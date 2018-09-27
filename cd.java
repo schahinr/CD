68a075cd1447e9b0c7e33baa2f489faa3ff9ed47
Schahin Rajab <schahin_94@hotmail.com>
2018-09-27 20:30:54 +0200

    Update cd.java

--- cd.java
+++ cd.java
@@ -40,106 +40,66 @@
 import java.util.*;
 
 public class cd{
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);			
-		int count = 0;
-		List<Integer> save = new ArrayList<Integer>();
-		
-		int n, m, zero1, zero2;
-		
-		n = 1;
-		m = 1;
-		
-		int[] jack;
-		int[] jill;
-			
-		while(n > 0 || m > 0){
-			count = 0;
-			
-			n = sc.nextInt();
-			m = sc.nextInt();
-			
-			if(n < 0 || m < 0)
-				System.exit(0);
-	    	if(n == 0 && m == 0)
-				break;
-			
-			jack = new int[n];
-			jill = new int[m];
-			
-			//enter values to array
-			if(n > 0){
-			for(int i = 0; i < n; i++)
-				jack[i] = sc.nextInt();
-			}
-		
-			if(m > 0){
-			for(int j = 0; j < m; j++)
-				jill[j] = sc.nextInt();
-			}
-			//debugging purposes
-			//System.out.println("Entered array: " + Arrays.toString(jack));
-			//System.out.println("Entered array: " + Arrays.toString(jill));
-		
-			//debugging time
-			//long startTime = System.currentTimeMillis();
-			
-			//sort arrays
-			if(n > 1)
-			Arrays.sort(jack);
-			if(m > 1)
-			Arrays.sort(jill);
-			
-			//debugging purposes
-			//System.out.println("Sorted array: " + Arrays.toString(jack));
-			//System.out.println("Sorted array: " + Arrays.toString(jill));
-			
-			//debugging time
-			//long stopTime = System.currentTimeMillis();
-			//long elapsedTime = stopTime - startTime;
-			//System.out.println("elapsed time after sort " + elapsedTime);
-		
-			//binary search to find common integers
-			
-			for(int s = 0; s < n; s++)
-			count = count + binarySearch(jack[s],jill,0,m-1);
-			
-			
-			save.add(count);
-			//debugging time
-			//stopTime = System.currentTimeMillis();
-			//elapsedTime = stopTime - startTime;
-			//System.out.println("elapsed time after search" + elapsedTime);
-			
-			jack = null;
-			jill = null;
-		}
-		
-		for(int klar = 0; klar < save.size(); klar++)
-			System.out.println(save.get(klar));
-	//closes main
-	}
-
-	public static int binarySearch(int value, int[] arr, int start, int end){
-			int len = end - start + 1;
-			int mid = start + (end - start) / 2;
-			if(len == 1){
-				if(arr[mid] == value)
-					return 1;
-				else
-					return 0;
-			}
-			
-			if (arr[mid] == value)
-				return 1;
-			else{
-				if(arr[mid] < value)
-					return binarySearch(value, arr, mid+1, end);
-				else
-					return binarySearch(value, arr, start, mid-1);
-				}
-	}
-	
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);            
+        List<Integer> save = new ArrayList<Integer>(); //arraylist to add number of common cds since add is O(1)
+        
+        int cd = 0;
+        int count = 0;
+        int n = 1;
+        int m = 1;
+        
+        HashSet<Integer> set = new HashSet<Integer>(1000000, 1.0f);
+            
+        while(n > 0 || m > 0){
+            count = 0;  //reset counter
+            
+            n = sc.nextInt();
+            m = sc.nextInt();
+            //debugging time
+            //long startTime = System.currentTimeMillis();
+            if(n < 0 || m < 0)
+                System.exit(0); //terminate program if a negative integer is entered
+            if(n == 0 && m == 0)
+                break;  //break out of while loop if 0 0 has been entered
+            
+            //enter values to hashset
+            if(n > 0){
+                for(int i = 0; i < n; i++){
+                    //debugging purposes
+                    /*for(int load = 0; load < 1000000; load++){
+                    cd = load;*/
+                    cd = sc.nextInt();
+                    set.add(cd);
+                    }
+            }
+        
+            if(m > 0){
+                for(int j = 0; j < m; j++){
+                    //debugging purposes
+                    /*for(int load = 0; load < 1000000; load++){
+                    cd = load;*/
+                    cd = sc.nextInt();
+                    if(set.contains(cd))    //checks if catalog number is already int hashset
+                        count++;
+                    }
+            }
+        
+            
+            save.add(count);
+    
+            set.clear();
+            //debugging time
+            /*long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            System.out.println("elapsed time from adding values to clearing hashset: " + elapsedTime);*/
+            
+        }
+        
+        for(int klar = 0; klar < save.size(); klar++)
+            System.out.println(save.get(klar));
+    //closes main
+    }
 //closes class
 }
